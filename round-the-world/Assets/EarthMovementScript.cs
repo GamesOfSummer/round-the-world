06e9c222a0e6f4f0bd119b24f977e339d73ff7b3
Summer <summer.d.howard@gmail.com>
2018-12-12 19:10:41 -0600

    90% done

---
+++ round-the-world/Assets/EarthMovementScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EarthMovementScript : MonoBehaviour
+{
+    float timeCounter = 0;
+    float radiusDistance = 0;
+
+    private void Start()
+    {
+        InvokeRepeating("ChangeRadius", 1.0F, 1.0F);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeCounter += radiusDistance * Time.deltaTime;
+        float x = Mathf.Cos(timeCounter);
+        float y = Mathf.Sin(timeCounter);
+        float z = 0;
+        transform.position = new Vector3(x, y, z);
+    }
+
+    void ChangeRadius()
+    {
+        radiusDistance = Random.Range(1, 10);
+    }
+
+}
---
+++ round-the-world/Assets/GameController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+    private bool paused;
+    private void Awake()
+    {
+
+        if (Application.isPlaying)
+        {
+            if (instance)
+            {
+                Debug.Log("Destroying irrelevant GameController instance");
+                Destroy(this);
+            }
+
+            instance = this;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            togglePause();
+
+    }
+
+    public void togglePause()
+    {
+        if (paused)
+        {
+            Time.timeScale = 1f;
+            paused = false;
+        }
+        else
+        {
+            Time.timeScale = 0f;
+            paused = true;
+        }
+    }
+
+
+}
